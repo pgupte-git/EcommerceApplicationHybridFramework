6cf9b286be2230a35a2a487b211f95326e4e6869
pratik <pgbitbucket@gmail.com>
2023-07-23 19:16:25 +0530

    Added the comment to cart page and added page object for cart page

---
+++ src/main/java/com/nopcommerce/pageobjects/CartPage.java
@@ -0,0 +1,9 @@
+package com.nopcommerce.pageobjects;
+
+import com.nopcommerce.utilities.Utility;
+
+public class CartPage extends Utility{
+	
+	// This class will consists of page locators and action methods to cart page
+
+}
--- src/test/java/com/nopcommerce/testcases/CartTest.java
+++ src/test/java/com/nopcommerce/testcases/CartTest.java
@@ -3,5 +3,7 @@ package com.nopcommerce.testcases;
 import com.nopcommerce.testutility.BaseTest2;
 
 public class CartTest extends BaseTest2 {
+	
+	// This is my comment regarding to cart page
 
 }
